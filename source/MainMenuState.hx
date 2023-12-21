2b42e8d86944c2c998f4da5c35d0fef13ec74d9e
Snirozu <snirozu788@gmail.com>
2023-12-21 17:21:24 +0100

    FUCK

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,7 +26,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychOnlineVersion:String = "0.3.1";
+	public static var psychOnlineVersion:String = "0.3.2";
 	public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
